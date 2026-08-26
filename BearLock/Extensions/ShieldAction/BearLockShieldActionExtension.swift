395389a0eec013b72be00549727003609326a0a7
hiroyoshii <orient110yh@gmail.com>
2026-08-27 00:52:20 +0900

    Fix shield action delegate token overrides

--- BearLock/Extensions/ShieldAction/BearLockShieldActionExtension.swift
+++ BearLock/Extensions/ShieldAction/BearLockShieldActionExtension.swift
@@ -4,7 +4,7 @@ import ManagedSettingsUI
 final class BearLockShieldActionExtension: ShieldActionDelegate {
     override func handle(
         action: ShieldAction,
-        for application: Application,
+        for application: ApplicationToken,
         completionHandler: @escaping (ShieldActionResponse) -> Void
     ) {
         completionHandler(.defer)
@@ -12,7 +12,7 @@ final class BearLockShieldActionExtension: ShieldActionDelegate {
 
     override func handle(
         action: ShieldAction,
-        for webDomain: WebDomain,
+        for webDomain: WebDomainToken,
         completionHandler: @escaping (ShieldActionResponse) -> Void
     ) {
         completionHandler(.defer)
@@ -20,7 +20,7 @@ final class BearLockShieldActionExtension: ShieldActionDelegate {
 
     override func handle(
         action: ShieldAction,
-        for category: ActivityCategory,
+        for category: ActivityCategoryToken,
         completionHandler: @escaping (ShieldActionResponse) -> Void
     ) {
         completionHandler(.defer)
